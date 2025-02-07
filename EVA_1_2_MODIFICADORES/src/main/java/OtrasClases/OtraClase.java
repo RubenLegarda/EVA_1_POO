a1fe7ab8594729fd77aa132dac4df2436683712b
RubenLegarda <159494601+RubenLegarda@users.noreply.github.com>
2025-02-06 18:30:48 -0600

    Add files via upload

---
+++ EVA_1_2_MODIFICADORES/src/main/java/OtrasClases/OtraClase.java
@@ -0,0 +1,11 @@
+package OtrasClases;
+
+public class OtraClase {
+    public int valorPublico;
+    private int valorPrivado;
+    int valorDefault;
+}
+
+class Prueba{
+    
+}
---
+++ EVA_1_2_MODIFICADORES/src/main/java/com/legrey/eva_1_2_modificadores/EVA_1_2_MODIFICADORES.java
@@ -0,0 +1,13 @@
+package com.legrey.eva_1_2_modificadores;
+
+import OtrasClases.OtraClase;
+
+public class EVA_1_2_MODIFICADORES {
+
+    public static void main(String[] args) {
+        OtraClase otraClase = new OtraClase(); 
+        otraClase.valorPublico
+        //Prueba prueba = new Prueba(); f
+        //Prueba no es visible, su modificador es default solo se puede utilizar en su propio paquete
+    }
+}
